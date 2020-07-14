44c75bdc9f92945a5d205daf3c23621c2e54c04e
hellohahello <1291482971@qq.com>
2020-07-14 20:07:36 +0800

    新增测试类1和2

---
+++ src/main/java/com/bishe/test/Test2.java
@@ -0,0 +1,12 @@
+package com.bishe.test;
+
+/**
+ * @auther: 翁筱寒
+ * @date: 2020/7/14 20:05
+ * @desc: //TODO
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("新增测试类2");
+    }
+}
